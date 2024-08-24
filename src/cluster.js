db7713dbeb645d465762dea432d4c7865ed4fb08
KalyaniBorase <157592070+KalyaniBorase@users.noreply.github.com>
2024-08-24 12:13:08 +0530

    cluster.js

---
+++ src/cluster.js
@@ -0,0 +1,16 @@
+const cluster = require('cluster');
+const os = require('os');
+
+if (cluster.isMaster) {
+    const numWorkers = 2;
+    for (let i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker) => {
+        console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
+        cluster.fork();
+    });
+} else {
+    require('./server');
+}
